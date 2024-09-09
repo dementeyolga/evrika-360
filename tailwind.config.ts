6a0eec473394caa8c9845777f882dccece01cb43
dementeyolga <dementeyolya@gmail.com>
2024-09-09 15:20:12 +0300

    core: setup tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,41 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      background: 'var(--background)',
+      foreground: 'var(--foreground)',
+      green: {
+        200: 'var(--green-200)',
+        500: 'var(--green-500)',
+      },
+      sky: {
+        200: 'var(--sky-200)',
+        500: 'var(--sky-500)',
+      },
+      orange: {
+        200: 'var(--orange-200)',
+        500: 'var(--orange-500)',
+      },
+      slate: {
+        200: 'var(--slate-200)',
+        500: 'var(--slate-500)',
+      },
+      white: 'var(--white)',
+      font: {
+        main: 'var(--main)',
+        contrast: 'var(--contrast)',
+        secondary: 'var(--secondary)',
+      },
+    },
     extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+      boxShadow: {
+        button: 'var(--shadow-button)',
       },
     },
   },
+
   plugins: [],
 }
 export default config
