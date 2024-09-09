f64dfd445503e7fa449d7914bbfa64ce9f62ac00
dementeyolga <dementeyolya@gmail.com>
2024-09-09 14:49:31 +0300

    core: setup project and font

---
+++ src/app/(content)/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { DM_Sans } from 'next/font/google'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Эврика 360 - Стратегии правильных решений',
+  description:
+    'Доверьте анализ и оценку качества работы сотрудников речевой аналитике сегодня',
+}
+
+const dmSans = DM_Sans({ subsets: ['latin'] })
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="ru">
+      <body className={`${dmSans.className} antialiased`}>{children}</body>
+    </html>
+  )
+}
