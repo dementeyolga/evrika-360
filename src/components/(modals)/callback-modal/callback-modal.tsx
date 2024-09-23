e7d83dd3be028fb68accdb2b5649400af8210e30
dementeyolga <dementeyolya@gmail.com>
2024-09-23 15:58:50 +0300

    feat: improve styles

--- src/components/(modals)/callback-modal/callback-modal.tsx
+++ src/components/(modals)/callback-modal/callback-modal.tsx
@@ -13,7 +13,7 @@ export const CallbackModal = ({
 }) => {
   return (
     <Modal reference={reference} onClick={onClick}>
-      <div className="relative w-fit h-fit my-[70px] mx-2.5 ">
+      <div className="relative w-fit my-[70px] mx-2.5 ">
         <button
           onClick={handleCloseModal}
           className="absolute left-1/2 -top-[30px] lg:-top-7 -translate-x-1/2 w-[50px] h-[50px] bg-foreground flex justify-center items-center rounded-full"
@@ -21,7 +21,7 @@ export const CallbackModal = ({
           <img src="/cross.svg" alt="" />
         </button>
 
-        <div className="relative mt-[30px] p-5 xl:p-10 flex flex-col justify-center max-w-[500px] w-full rounded-section-mobile lg:rounded-section-sm overflow-hidden bg-foreground">
+        <div className="relative mt-[30px] p-5 xl:p-10 flex flex-col max-w-[500px] max-h-[calc(100dvh_-_130px)] w-full rounded-section-mobile lg:rounded-section-sm overflow-y-auto bg-foreground">
           <p className="text-center lg:text-left mb-4 xl:mb-6 text-lg xl:text-xl xl:text-2xl">
             Введите свои данные в форму и{' '}
             <b>закажите звонок нашего специалиста для консультации </b>
